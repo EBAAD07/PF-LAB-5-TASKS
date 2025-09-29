a4bbe308885e26be481551056d57940d9bd51782
EBAAD07 <K250034@nu.edu.pk>
2025-09-29 23:11:45 +0500

    Add files via upload
    # 5TH TASK

---
+++ relational op.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter a = ");
+	scanf("%d",&a);
+	printf("Enter b = ");
+	scanf("%d",&b);
+
+	printf("\na==b : %d",a==b);
+		printf("\na>b : %d ",a>b);
+			printf("\na<b : %d",a<b);
+				printf("\na!=b : %d",a!=b);
+					printf("\na>=b : %d",a>=b);
+						printf("\na<=b : %d",a<=b);
+	return 0;					
+}
